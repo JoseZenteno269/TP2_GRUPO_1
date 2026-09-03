7382d8381b68d870ede520761948e35d4d6a9da1
jere2003-lab <gabrieltortora450@gmail.com>
2026-09-02 22:45:50 -0300

    Agrego la clase Edificio con sus cositas

---
+++ src/Ejercicio2/Edificio.java
@@ -0,0 +1,34 @@
+package Ejercicio2;
+
+public class Edificio {
+
+	private double superficie;
+	
+	//Constructor
+	
+	public Edificio(double Superficie)
+	{
+		this.superficie = Superficie;
+	}
+
+	// Metodo
+	public double GetSuperFicieEdificio()
+	{
+		return superficie;
+	}
+	
+	// Setters y getters
+	
+	public double getSuperficie() {
+		return superficie;
+	}
+
+
+	public void setSuperficie(double superficie) {
+		this.superficie = superficie;
+	}
+	
+	
+	
+	
+}
